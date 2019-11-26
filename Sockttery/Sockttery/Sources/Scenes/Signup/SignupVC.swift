f02d8ca25b7c18d0ea79e4a526fac5e06e9a20f4
khyunjiee <khyunjiee@gmail.com>
2019-11-26 23:36:14 +0900

    [UPDATE] add basic network

---
+++ Sockttery/Sockttery/Sources/Scenes/Signup/SignupVC.swift
@@ -0,0 +1,26 @@
+//
+//  SignupVC.swift
+//  Sockttery
+//
+//  Created by 김현지 on 2019/11/26.
+//  Copyright © 2019 김현지. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    @IBOutlet var userId: UITextField!
+    @IBOutlet var userPw: UITextField!
+    @IBOutlet var signupBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func signupAction(_ sender: Any) {
+        
+    }
+    
+}
